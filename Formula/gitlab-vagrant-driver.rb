c2fafbd441d14955fd5993651421a56611593ae3
Loïc PÉRON <loic.peron@gmail.com>
2026-06-14 22:07:46 +0200

    gitlab-vagrant-driver: version 0.0.3

--- Formula/gitlab-vagrant-driver.rb
+++ Formula/gitlab-vagrant-driver.rb
@@ -1,8 +1,8 @@
 class GitlabVagrantDriver < Formula
   desc "Custom GitLab Runner driver that provisions CI jobs in Vagrant VMs"
   homepage "https://github.com/nicerloop/gitlab-vagrant-driver"
-  url "https://github.com/nicerloop/gitlab-vagrant-driver/archive/refs/tags/0.0.2.tar.gz"
-  sha256 "b18335a3ae732fc3cf265d25401daa8a3c479cad770e8119438c99b359a8717c"
+  url "https://github.com/nicerloop/gitlab-vagrant-driver/archive/refs/tags/0.0.3.tar.gz"
+  sha256 "bd4581600e3657feddcac71fcfcfb741812512ecbc9e9a73cb63b6e03e4a4b71"
   license "MIT"
 
   def install
